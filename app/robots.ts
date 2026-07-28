93254cf370c8c3c8495af82be58fd8fc1e90ece2
aslanmediadigital-maker <aslanmediadigital@gmail.com>
2026-07-28 12:03:52 +0300

    Fix robots file

--- app/robots.ts
+++ app/robots.ts
@@ -1,30 +1,13 @@
 import type { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: "https://oncebak.com",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 1,
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/admin"],
     },
-    {
-      url: "https://oncebak.com/hakkimizda",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://oncebak.com/iletisim",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://oncebak.com/gizlilik",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.5,
-    },
-  ];
+    sitemap: "https://oncebak.com/sitemap.xml",
+    host: "https://oncebak.com",
+  };
 }
